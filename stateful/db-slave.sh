cd1445cea5e4763492a0a5dbc8640d6c1ad428b7
elynn <elynn@devstack.7610>
2015-01-14 21:53:52 +0800

    Using url to retrieve files

--- stateful/db-slave.sh
+++ stateful/db-slave.sh
@@ -5,7 +5,9 @@ sed -i "s/.*wsrep_node_address=.*/wsrep_node_address=$host/" /etc/my.cnf.d/galer
 sed -i "s/.*wsrep_provider=.*/wsrep_provider=\/usr\/lib64\/galera\/libgalera_smm.so/" /etc/my.cnf.d/galera.cnf
 sed -i "s/.*wsrep_sst_auth=.*/wsrep_sst_auth=sst:Passw0rd/" /etc/my.cnf.d/galera.cnf
 systemctl enable mariadb.service
-systemctl restart mariadb.service
+systemctl stop mariadb.service
+sleep 60s
+systemctl start mariadb.service
 #mysqladmin -u root password $db_rootpassword
 #cat << EOF | mysql -u root --password=$db_rootpassword
 cat << EOF | mysql -u root
@@ -13,8 +15,3 @@ GRANT ALL PRIVILEGES ON *.* TO "sst"@"%" IDENTIFIED BY "Passw0rd";
 FLUSH PRIVILEGES;
 EXIT
 EOF
-#params:
-#$db_rootpassword: {get_attr: [database_root_password, value]}
-#$db_name: {get_param: database_name}
-#$db_user: {get_param: database_user}
-#$db_password: {get_attr: [database_password, value]}
