6023f6d19e30889f9fca1a095319e31b13906d9e
elynn <elynn@elynn-78.cn.ibm.com>
2015-11-06 14:50:21 +0800

    update

--- acdc/setup.sh
+++ acdc/setup.sh
@@ -4,8 +4,7 @@ set -x
 echo 'Install git' > /tmp/devstack
 if [[ -e /etc/redhat-release ]]; then
   iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-  service iptables save
-  yum install -y git
+  yum install -y git postgresql-devel
 fi
 su - stack -c 'bash /tmp/install.sh' << END
 password
