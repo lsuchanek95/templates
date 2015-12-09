62e976cbad0d6ca055b26ba02f8f1f87c18624c0
elynn <elynn@localhost.localdomain>
2015-12-09 20:21:31 +0800

    Update

--- acdc/update_local.sh
+++ acdc/update_local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-curl -O https://raw.githubusercontent.com/lynic/config/master/devstack/local.conf
+curl -o $SCRIPT_DIR/local.conf https://raw.githubusercontent.com/lynic/config/master/devstack/local.conf
