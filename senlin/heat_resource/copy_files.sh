a7af4c7956e8586a036ad81fde662905e72e00d3
elynn <elynn@localhost.localdomain>
2016-03-09 13:13:48 +0800

    Update

--- senlin/heat_resource/copy_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-
-cp -f /opt/stack/heat/heat/engine/clients/os/senlin.py client/
-cp -f /opt/stack/heat/heat/engine/resources/openstack/senlin/*.py resources/senlin/
-cp -f /opt/stack/heat/setup.cfg ./
