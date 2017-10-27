6863ab3ef5a7c25acf5150050e94cf9ba0e07556
Ethan Lynn <xuanlangjian@gmail.com>
2017-10-27 07:03:24 -0500

    Update monitor.sh

--- k8s/monitor.sh
+++ k8s/monitor.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 set -x
 
-NODES=`kubectl get nodes|grep -v 'master'`
-TOTAL_COUNT=$((`echo "$NODES"| wc -l` - 1))
-READY_COUNT=$((`echo "$NODES"| grep 'Ready' | wc -l`))
-NOTREADY_COUNT=$((`echo "$NODES"| grep 'NotReady' | wc -l`))
-MEM_COUNT=$((`echo "$NODES"|grep 'MemoryPressure' | wc -l`))
+if [[ ! -f '/opt/scaleout_url.txt' ]]; then
+  echo "`date` no scaleout_url.txt"
+  exit 0
+fi
 
-HEALTHY_PERCENT=$((($TOTAL_COUNT-$NOTREADY_COUNT)*100/$TOTAL_COUNT))
+# NODES=`kubectl get nodes|grep -v 'master'`
+# TOTAL_COUNT=$((`echo "$NODES"| wc -l` - 1))
+# READY_COUNT=$((`echo "$NODES"| grep 'Ready' | wc -l`))
+# NOTREADY_COUNT=$((`echo "$NODES"| grep 'NotReady' | wc -l`)) 
+# MEMP_COUNT=$((`echo "$NODES"|grep 'MemoryPressure' | wc -l`))
+# HEALTHY_PERCENT=$((($TOTAL_COUNT-$NOTREADY_COUNT)*100/$TOTAL_COUNT))
 
+SCALEOUT_URL=`cat /opt/scaleout_url.txt`
 
 MEMS=`kubectl top  node|sed -n '1!p'|awk '{print $5}'|sed 's/%//g'`
 MEM_LINES=`echo $MEMS|wc -l`
 SUM_MEM=`echo $MEMS |awk '{s+=$1} END {print s}'`
 AVG_MEM=$(($SUM_MEM/$MEM_LINES))
-if [[ -f '/opt/scaleout_url.txt' ]] && [[ $AVG_MEM -gt 80 ]];then
-  echo "alarm"
+
+if [[ $AVG_MEM -gt 80 ]]; then
+  echo -n "`date` "
+  echo "AVERAGE MEMORY: $AVG_MEM"
+  curl -X POST $SCALEOUT_URL
 fi
