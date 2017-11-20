5c90388efb6dfc709b4f55e0e348b1fecfd15060
Ethan Lynn <xuanlangjian@gmail.com>
2017-11-20 17:51:04 +0800

    update

--- scripts/config-ip.sh
+++ scripts/config-ip.sh
@@ -6,7 +6,7 @@ IP=`echo $NET_INFO|cut -d'/' -f1`
 PREFIX=`echo $NET_INFO|cut -d'/' -f2`
 GATEWAY=`ip route |grep default|awk '{print $3}'`
 
-sed -i "2i/IPADDR=$IP" /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
-sed -i "2i/PREFIX=$PREFIX" /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
-sed -i "2i/GATEWAY=$GATEWAY" /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
+sed -i "2i IPADDR=$IP" /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
+sed -i "2i PREFIX=$PREFIX" /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
+sed -i "2i GATEWAY=$GATEWAY" /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
 sed -i 's/^BOOTPROTO=.*/BOOTPROTO=none/' /etc/sysconfig/network-scripts/ifcfg-$NIC_NAME
